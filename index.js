ec5136122ffab7f3c2f43a7d8a8a1bdc04edcd27
Brian Choi <b.choi562@gmail.com>
2021-02-11 20:26:50 -0800

    module 4

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // require your server and launch it here
+const dotenv = require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname,"client/build")))
+
+app.use("/api/*", (_,res)=>{
+    res.json({data:"THE API IS ALIVE"})
+})
+
+app.use("*",(_,res)=>{
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+})
+
 const server = require("./api/server.js");
-const port = 4000
 
 server.listen(port, () => {
-    console.log(`Server running on http://localhost:4000 `);
+    console.log(`Server running on ${port} `);
 });
 
